f8ee0b9a20446ecfcd2589da340bdf2f4202e3bc
marciotnt2 <mad.max.215@gmail.com>
2020-06-14 21:53:13 -0300

    fist commit

---
+++ app/src/main/java/com/leda/ccr/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.leda.ccr.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
